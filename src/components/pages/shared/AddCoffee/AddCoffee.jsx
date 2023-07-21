ed1bff38be03cf8da83c77056f2606be523bf40e
PritamDutta <webdevpritam7498@gmail.com>
2023-07-21 14:16:50 +0600

    coffee data send successfully to db

--- src/components/pages/shared/AddCoffee/AddCoffee.jsx
+++ src/components/pages/shared/AddCoffee/AddCoffee.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { BsArrowLeft } from "react-icons/bs";
 
 const AddCoffee = () => {
-  const handleAddCoffee = event =>{
+  const handleAddCoffee = (event) => {
     event.preventDefault();
     const form = event.target;
     const name = form.name.value;
@@ -13,25 +13,41 @@ const AddCoffee = () => {
     const detail = form.detail.value;
     const category = form.category.value;
 
-    const  newCoffee = {name, supplier, quantity, photo, taste, detail, category};
+    const newCoffee = {
+      name,
+      supplier,
+      category,
+      quantity,
+      taste,
+      detail,
+      photo,
+    };
     console.log(newCoffee);
 
     // send data to the server
-    fetch('http://localhost:5000/coffee',{
-      method:'POST',
-      headers:{
-        "content-type" : "application/json"
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
       },
-      body:JSON.stringify(newCoffee)
+      body: JSON.stringify(newCoffee),
     })
-    .then(res => res.send())
-    .then(data =>{
-      console.log(data);
-    })
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          alert("Coffee Added Successfully!");
+        }
+      });
+  };
   return (
     <div className="bg-[color:#F4F3F0] py-5 mb-20 ">
-       <Link to="/" className="flex align-middle items-center gap-3  my-5 ml-40 font-semibold text-[color:#374151]"><BsArrowLeft/> Back to home</Link>
+      <Link
+        to="/"
+        className="flex align-middle items-center gap-3  my-5 ml-40 font-semibold text-[color:#374151]"
+      >
+        <BsArrowLeft /> Back to home
+      </Link>
       <div className="text-center w-3/5 mx-auto">
         <h3 className="text-2xl font-bold my-4 text-orange-700 drop-shadow-lg">
           Add New Coffee
@@ -117,24 +133,27 @@ const AddCoffee = () => {
           </div>
         </div>
         <div className="form-control  w-4/5 px-5 mx-auto">
-              <label className="label">
-                <span className="label-text">photo Url</span>
-              </label>
-              <input
-                type="text"
-                name="photo"
-                placeholder="Enter photo Url"
-                className="input input-bordered"
-              />
-            </div>
-      <input type="submit" className="btn bg-[color:#DFA878] text-bold hover:bg-[color:#BA704F]  w-[61.3rem] mx-auto flex my-10" value="Add Coffee" />
+          <label className="label">
+            <span className="label-text">photo Url</span>
+          </label>
+          <input
+            type="text"
+            name="photo"
+            placeholder="Enter photo Url"
+            className="input input-bordered"
+          />
+        </div>
+        <input
+          type="submit"
+          className="btn bg-[color:#DFA878] text-bold hover:bg-[color:#BA704F]  w-[61.3rem] mx-auto flex my-10"
+          value="Add Coffee"
+        />
       </form>
     </div>
   );
 };
 
 export default AddCoffee;
-
 
 /* 
 ----img link--------
